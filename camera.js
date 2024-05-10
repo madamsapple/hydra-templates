b74e74e3a0a30683bffafba262300659e63d5a44
Iti Gupta <itig2003@gmail.com>
2024-05-10 15:59:03 +0100

    Updated camera.js

--- camera.js
+++ camera.js
@@ -2,6 +2,7 @@ let hydraCanvas1 = document.getElementById("hydra-bg1");
 // set small size to avoid high resource demand:
 hydraCanvas1.width  = Math.min(window.innerWidth  / 2, 1280);
 hydraCanvas1.height = Math.min(window.innerHeight / 2, 720);
+console.log("div initialised");
 
 const hydra1 = new Hydra({
   canvas: hydraCanvas1,
@@ -9,8 +10,15 @@ const hydra1 = new Hydra({
   detectAudio: true,
   enableStreamCapture: false,
 });
+console.log("hydra initialised");
 
-const h2 = hydra1.synth;
+const h1 = hydra1.synth;
+h1.s0.initCam() //initialize webcam as external source 's0'
+//h1.src(h1.s0).invert(1).out(h1.o1);
+console.log("new camera init");
 
-h2.s0.initCam() //initialize webcam as external source 's0'
-src(h2.s0).invert(1).out(h2.o1)
+h1.src(h1.s0)
+.color(1, 1, 1)      // Ensure the color is set to maximum (white)
+.invert()            // Apply the invert effect
+.out(h1.o1);         // Output to the first buffer
+console.log("h1 src");
